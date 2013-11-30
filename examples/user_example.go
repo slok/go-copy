e7f5481346823c64fa41f379b482762c45ed5ce6
Xabier Larrakoetxea <slok69@gmail.com>
2013-11-30 12:39:25 +0100

    Added Use retrieval example

---
+++ examples/user_example.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/slok/go-copy/copy"
+	"math"
+	"os"
+)
+
+func main() {
+	//Prepare the neccesary data
+	appToken := os.Getenv("APP_TOKEN")
+	appSecret := os.Getenv("APP_SECRET")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	accessSecret := os.Getenv("ACCESS_SECRET")
+
+	// Create the client
+	client, err := copy.NewDefaultClient(appToken, appSecret, accessToken, accessSecret)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Could not create the client, review the auth params")
+		os.Exit(-1)
+	}
+
+	//Create the service (in this case for a user)
+	userService := copy.NewUserService(client)
+
+	//Play with the lib :)
+	user, err := userService.Get()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Could not retrieve the user")
+		os.Exit(-1)
+	}
+
+	byteToMegabyte := math.Pow(1024, 2)
+	fmt.Printf("User: %v %v\n", user.FirstName, user.LastName)
+	fmt.Printf("Email: %v\n", user.Email)
+	fmt.Printf("Stored(MB): %G of %G\n",
+		float64(user.Storage.Used)/byteToMegabyte, float64(user.Storage.Quota)/byteToMegabyte)
+
+}
