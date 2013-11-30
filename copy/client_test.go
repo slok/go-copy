d6c699166e07a3e3501e966a099ccbf5ffb19418
Xabier Larrakoetxea <slok69@gmail.com>
2013-11-30 08:33:39 +0100

    Test more things \o/

--- copy/client_test.go
+++ copy/client_test.go
@@ -28,6 +28,8 @@ func setup(t *testing.T) {
 	}
 }
 
+// ---------Global testing utils-----------------------------------------------
+
 // Cleans all the setup of the test
 func tearDown() {
 	server.Close()
@@ -49,4 +51,64 @@ func NewTestClient() (*Client, error) {
 
 	serverUrl, _ := url.Parse(server.URL)
 	return NewClient(nil, serverUrl.String(), appToken, appSecret, accessToken, accessSecret)
+}
+
+// -----------Client tests-----------------------------------------------------
+
+// Tests that a client is created fine
+func TestClientCreation(t *testing.T) {
+	appToken = os.Getenv(appTokenEnv)
+	appSecret = os.Getenv(appSecretEnv)
+	accessToken = os.Getenv(accessTokenEnv)
+	accessSecret = os.Getenv(accessSecretEnv)
+
+	_, err := NewClient(nil, "http://resources/fake", appToken, appSecret, accessToken, accessSecret)
+
+	if err != nil {
+		t.Error("Error creating a client")
+	}
+
+}
+
+// Tests the creation of the client with errors
+func TestClientWrongParams(t *testing.T) {
+	appToken = os.Getenv(appTokenEnv)
+	appSecret = os.Getenv(appSecretEnv)
+	accessToken = os.Getenv(accessTokenEnv)
+	accessSecret = ""
+
+	_, err := NewClient(nil, "http://resources/fake", appToken, appSecret, accessToken, accessSecret)
+
+	if err == nil {
+		t.Error("Should be an error when creating the client")
+	}
+}
+
+// Tests a creation default client
+func TestDefaultClientCreation(t *testing.T) {
+	appToken = os.Getenv(appTokenEnv)
+	appSecret = os.Getenv(appSecretEnv)
+	accessToken = os.Getenv(accessTokenEnv)
+	accessSecret = os.Getenv(accessSecretEnv)
+
+	_, err := NewDefaultClient(appToken, appSecret, accessToken, accessSecret)
+
+	if err != nil {
+		t.Error("Error creating a default client")
+	}
+
+}
+
+// Creates the client with error
+func TestDefaultClientWrongParams(t *testing.T) {
+	appToken = os.Getenv(appTokenEnv)
+	appSecret = os.Getenv(appSecretEnv)
+	accessToken = os.Getenv(accessTokenEnv)
+	accessSecret = ""
+
+	_, err := NewDefaultClient(appToken, appSecret, accessToken, accessSecret)
+
+	if err == nil {
+		t.Error("Should be an error when creating the default client")
+	}
 }
