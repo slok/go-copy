440374f80ec8dff9ff4494fc0ad3ed89e8d01806
Xabier Larrakoetxea <slok69@gmail.com>
2013-12-17 22:29:50 +0100

    Added link methods to implement

--- copy/links.go
+++ copy/links.go
@@ -21,3 +21,31 @@ func NewLinkService(client *Client) *LinkService {
 	fs.client = client
 	return fs
 }
+
+func (linkService *LinkService) GetLink(token string) (*Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) GetLinks() ([]Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) CreateLink(name string, paths []string, public bool) (*Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) AddPaths(token string, paths []string) (*Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) AddRecipients(token string, recipients []Recipient) (*Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) DeleteLink(token string) (*Meta, error) {
+	return nil, nil
+}
+
+func (linkService *LinkService) GetFilesMetaFromLink(token string) (*Meta, error) {
+	return nil, nil
+}
